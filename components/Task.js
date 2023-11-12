ad19eb0d134cd84316ebcacc5a8bd8117223bafa
datorrado <datorrado@gmail.com>
2023-11-12 18:19:44 +0100

    Add reusable component for Tasks

---
+++ components/Task.js
@@ -0,0 +1,14 @@
+import React  from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const Task = (props) => {
+    <View> 
+        <Text>{props.text}</Text>
+    </View>
+
+}
+
+const styles = StyleSheet.create({
+});
+
+export default Task;
