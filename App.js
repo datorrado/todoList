32d42c7f4e0cfe23159ea2d301c12c50f9180447
datorrado <datorrado@gmail.com>
2023-11-12 18:46:29 +0100

    Add margin to Title

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/task';
 
 export default function App() {
   return (
@@ -8,6 +9,9 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's tasks</Text>
       
         <View style={styles.items}>
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
+          <Task text={'Task 3'} />
         </View>
       </View>
     </View>
@@ -27,5 +31,8 @@ const styles = StyleSheet.create({
   tasksWrapper: {
     paddingTop: 80,
     paddingHorizontal: 20,
-  }
+  },
+  items: {
+    marginTop: 30,
+  },
 });
